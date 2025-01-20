a28bf286a147ed307b1d8e9e29338ce25b2751cd
VCplus-512 <vincentcn512@gmail.com>
2025-01-20 16:18:35 -0500

    initial commit

---
+++ src/main/java/vcplus/hougu/HouguClient.java
@@ -0,0 +1,11 @@
+package vcplus.hougu;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HouguClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient(){
+
+    }
+
+}
